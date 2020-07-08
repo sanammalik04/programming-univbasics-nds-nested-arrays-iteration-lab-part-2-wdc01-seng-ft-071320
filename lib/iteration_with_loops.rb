b1c43f228f3978b186df5fb0d1654699072f7e32
Sanam Malik <sanam.malik04@gmail.com>
2020-07-08 20:04:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,14 +1,16 @@
 def find_min_in_nested_arrays(src)
   row_index = 0
+  minimumValues = []
   while row_index < src.count do
     element_index = 0
-    minimum = infinity
+    minimum = Float::INFINITY
     while element_index < src[row_index].count do
       if src[row_index][element_index]< minimum then
         minimum = src[row_index][element_index]
       end
       element_index += 1
     end
+    minimumValues[row_index] = minimum
     row_index += 1
 end
   # src will be an array of arrays of integers
